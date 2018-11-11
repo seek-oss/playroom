0daac7abde3972d3e177a79b6ed489648108631c
Jack Hurley <jack.hurley23@gmail.com>
2018-11-11 18:32:06 +1100

    refactor(WindowPortal): Ensure React can capture events in portal
    The previous implementation meant that any events (i.e onClicks) weren't being surfaced to the main
    application window

--- src/Playroom/WindowPortal/index.js
+++ src/Playroom/WindowPortal/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
-function copyStyles(sourceDoc, targetDoc) {
+const copyStyles = (sourceDoc, targetDoc) => {
   Array.from(sourceDoc.styleSheets).forEach(styleSheet => {
     if (styleSheet.cssRules) {
       // true for inline styles
@@ -21,44 +22,56 @@ function copyStyles(sourceDoc, targetDoc) {
       targetDoc.head.appendChild(newLinkEl);
     }
   });
-}
+};
 
 export default class WindowPortal extends React.PureComponent {
+  static propTypes = {
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    onClose: PropTypes.func.isRequired,
+    children: PropTypes.node.isRequired
+  };
+
   constructor(props) {
     super(props);
-    // STEP 1: create a container <div>
-    this.containerEl = document.createElement('div');
-    this.externalWindow = null;
+    this.state = { externalWindow: null, containerDiv: null };
   }
 
   componentDidMount() {
-    // STEP 3: open a new browser window and store a reference to it
-    this.externalWindow = window.open(
+    this.createWindow();
+  }
+
+  componentWillUnmount() {
+    const { externalWindow } = this.state;
+
+    if (externalWindow) {
+      externalWindow.close();
+    }
+  }
+
+  createWindow = () => {
+    const containerDiv = document.createElement('div');
+    const externalWindow = window.open(
       '',
       '',
       `width=${this.props.width},height=${this.props.height},left=200,top=200`
     );
 
-    // STEP 4: append the container <div> (that has props.children appended to it) to the body of the new window
-    this.externalWindow.document.body.appendChild(this.containerEl);
+    externalWindow.document.title = 'Playroom editor';
+    externalWindow.document.body.appendChild(containerDiv);
+    externalWindow.addEventListener('beforeunload', this.props.onClose);
 
-    this.externalWindow.document.title = 'Playroom editor';
-
-    copyStyles(document, this.externalWindow.document);
-
-    this.externalWindow.addEventListener('beforeunload', () =>
-      this.props.onClose()
-    );
-  }
-
-  componentWillUnmount() {
-    // STEP 5: This will fire when this.state.showWindowPortal in the parent component becomes false
-    // So we tidy up by closing the window
-    this.externalWindow.close();
-  }
+    copyStyles(document, externalWindow.document);
+    this.setState({ externalWindow, containerDiv });
+  };
 
   render() {
-    // STEP 2: append props.children to the container <div> that isn't mounted anywhere yet
-    return ReactDOM.createPortal(this.props.children, this.containerEl);
+    const { containerDiv } = this.state;
+
+    if (!containerDiv) {
+      return null;
+    }
+
+    return ReactDOM.createPortal(this.props.children, containerDiv);
   }
 }
