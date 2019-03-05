3338e05f30633edc4dca50d26dd60c2da4e627d0
Oliver Gassman <oliver.gassman@anz.com>
2019-03-05 17:13:07 +1100

    fix(linting): Forgot to remove reference to Component

--- src/Playroom/Preview/Preview.js
+++ src/Playroom/Preview/Preview.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Frame, { FrameContextConsumer } from 'react-frame-component';
 import CollectStyles from '../CollectStyles/CollectStyles';
