49ea2585f110d828e85e9498ee81851288318dc0
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2019-03-28 13:33:01 +1100

    feat: Add hot module reloading for components and themes (#53)

---
+++ src/Playroom/frameComponent.js
@@ -0,0 +1,4 @@
+/* eslint-disable-next-line import/no-unresolved */
+const frameComponent = require('__PLAYROOM_ALIAS__FRAME_COMPONENT__');
+
+module.exports = frameComponent.default || frameComponent;
