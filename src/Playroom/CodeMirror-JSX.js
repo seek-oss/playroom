c97b2230202868e2f743387010fcad21df37ea9e
Andrew Lisowski <lisowski54@gmail.com>
2019-03-29 00:55:39 -0700

    use lit-html for rendering the tooltip

--- src/Playroom/CodeMirror-JSX.js
+++ src/Playroom/CodeMirror-JSX.js
@@ -1,85 +1,52 @@
 /* eslint-disable new-cap */
 
 import styles from './CodeMirror-JSX.less';
+import { html, render } from 'lit-html';
 
-function elt(tagname, cls) {
-  const e = document.createElement(tagname);
-
-  if (cls) {
-    e.className = cls;
+function getTypeColor(data) {
+  if (data.type === 'boolean') {
+    return 'rebeccapurple';
   }
 
-  for (let i = 2; i < arguments.length; ++i) {
-    let element = arguments[i];
-
-    if (typeof element === 'string') {
-      element = document.createTextNode(element);
-    }
-
-    e.appendChild(element);
+  if (data.type === 'string' || data.values.length > 0) {
+    return 'darkred';
   }
 
-  return e;
+  if (data.type === 'number') {
+    return 'steelblue';
+  }
 }
 
-function makeTooltip(x, y, data) {
-  const content = [elt('span', null, data.description)];
+function makeTooltip(data) {
+  const required = data.required
+    ? html`
+        <span class=${styles.required}>ⓘ</span>
+      `
+    : '';
+  const defaultValue =
+    data.default !== null && typeof data.default !== 'undefined'
+      ? html`
+          <div class=${styles.default}>
+            <span class=${styles.defaultLabel}>Default:</span>
+            <span style="color:${getTypeColor(data)}">${data.default}</span>
+          </div>
+        `
+      : '';
+  const type =
+    data.type !== null && typeof data.type !== 'undefined'
+      ? html`
+          <div class=${styles.type}>
+            <span class=${styles.defaultLabel}>Type:</span>
+            <span>${data.type}</span>
+          </div>
+        `
+      : '';
 
-  if (data.required) {
-    content.unshift(elt('span', styles.required, 'ⓘ'));
-  }
-
-  if (data.default !== null && typeof data.default !== 'undefined') {
-    const value = elt('span', null, data.default);
-
-    if (data.type === 'boolean') {
-      value.style.color = 'rebeccapurple';
-    }
-
-    if (data.type === 'string' || data.values.length > 0) {
-      value.style.color = 'darkred';
-    }
-
-    if (data.type === 'number') {
-      value.style.color = 'steelblue';
-    }
-
-    content.push(
-      elt(
-        'div',
-        styles.default,
-        elt('span', styles.defaultLabel, 'Default:'),
-        value
-      )
-    );
-  }
-
-  if (data.type !== null && typeof data.type !== 'undefined') {
-    content.push(
-      elt(
-        'div',
-        styles.default,
-        elt('span', styles.defaultLabel, 'Type:'),
-        elt('span', null, data.type)
-      )
-    );
-  }
-
-  const node = elt('div', styles.tooltip, ...content);
-
-  node.style.left = `${x}px`;
-  node.style.top = `${y}px`;
-
-  document.body.appendChild(node);
-  return node;
-}
-
-function remove(node) {
-  const p = node && node.parentNode;
-
-  if (p) {
-    p.removeChild(node);
-  }
+  return html`
+    <div>
+      ${required} <span>${data.description}</span> ${defaultValue} ${type}
+    </div>
+  `;
 }
 
 function prepareSchema(tags) {
@@ -87,6 +54,10 @@ function prepareSchema(tags) {
     all[tag] = {
       ...tags[tag],
       attrs: Object.keys(tags[tag].attrs).reduce((allAttrs, attr) => {
+        if (attr === 'component_description') {
+          return allAttrs;
+        }
+
         allAttrs[attr] = tags[tag].attrs[attr].values;
         return allAttrs;
       }, {})
@@ -106,10 +77,11 @@ export default function getHints(cm, options) {
     })
   );
 
-  let tooltip = null;
+  const container = document.createElement('div');
+  document.body.appendChild(container);
 
-  CodeMirror.on(hint, 'close', () => remove(tooltip));
-  CodeMirror.on(hint, 'update', () => remove(tooltip));
+  CodeMirror.on(hint, 'close', () => container.remove());
+  CodeMirror.on(hint, 'update', () => container.remove());
   CodeMirror.on(hint, 'select', (data, node) => {
     const cur = cm.getCursor();
     const token = cm.getTokenAt(cur);
@@ -134,15 +106,20 @@ export default function getHints(cm, options) {
       };
     }
 
-    remove(tooltip);
+    container.remove();
 
     if (attr && attr.description) {
-      tooltip = makeTooltip(
-        node.parentNode.getBoundingClientRect().right + window.pageXOffset,
-        node.getBoundingClientRect().top + window.pageYOffset,
-        attr
-      );
-      tooltip.className += ' hint-doc';
+      const tooltip = makeTooltip(attr);
+      const x =
+        node.parentNode.getBoundingClientRect().right + window.pageXOffset;
+      const y = node.getBoundingClientRect().top + window.pageYOffset;
+
+      container.style.left = `${x}px`;
+      container.style.top = `${y}px`;
+      container.className = styles.tooltip;
+      document.body.appendChild(container);
+
+      render(tooltip, container);
     }
   });
 
