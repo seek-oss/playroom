df58fa8b524089605309817fb8937ba954b3f2a5
michaeltaranto <michael.taranto@gmail.com>
2019-07-25 20:42:15 +1000

    fix: Resolve prettier formatting error

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,10 +1,17 @@
+const getCodeEditor = () => cy.get('.react-codemirror2');
+
 export const typeCode = code =>
-  cy
-    .get('.CodeMirror')
+  getCodeEditor()
     .click()
     .focused()
     .clear({ force: true })
     .type(code, { force: true, delay: 100 })
+    .wait(1000);
+
+export const formatCode = () =>
+  getCodeEditor()
+    .focused()
+    .type('{meta}s')
     .wait(1000);
 
 export const assertFrameContains = async text => {
@@ -16,6 +23,10 @@ export const assertFrameContains = async text => {
     .contains(text);
 };
 
-export const assertCodePaneContains = async text => {
-  return cy.get('.react-codemirror2').contains(text);
-};
+export const assertCodePaneContains = async text =>
+  getCodeEditor().contains(text);
+
+export const assertCodePaneLineCount = async lines =>
+  getCodeEditor().within(() =>
+    cy.get('.CodeMirror-line').should('have.length', lines)
+  );
