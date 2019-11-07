d4a24aa5b9f7a31811dff8453a25a35e46d5d899
dylankil <dylankil@microsoft.com>
2019-11-06 17:16:11 -0800

    Initial commit of Microsoft's Office UI Fabric React example in Playroom.

---
+++ examples/office-ui-fabric-react/playroom/FrameComponent.js
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import React from 'react';
+import { Fabric } from 'office-ui-fabric-react/lib/Fabric';
+import { ThemeProvider } from '@uifabric/foundation';
+
+export default ({ theme, children }) => (
+  <Fabric>
+    <ThemeProvider theme={theme}>
+      {children}
+    </ThemeProvider>
+  </Fabric>
+);
---
+++ examples/office-ui-fabric-react/themes/index.ts
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const styling = require('@uifabric/styling');
+
+styling.loadTheme({
+    palette: {
+        themePrimary: '#0078d4',
+        themeLighterAlt: '#eff6fc',
+        themeLighter: '#deecf9',
+        themeLight: '#c7e0f4',
+        themeTertiary: '#71afe5',
+        themeSecondary: '#2b88d8',
+        themeDarkAlt: '#106ebe',
+        themeDark: '#005a9e',
+        themeDarker: '#004578',
+        neutralLighterAlt: '#f8f8f8',
+        neutralLighter: '#f4f4f4',
+        neutralLight: '#eaeaea',
+        neutralQuaternaryAlt: '#dadada',
+        neutralQuaternary: '#d0d0d0',
+        neutralTertiaryAlt: '#c8c8c8',
+        neutralTertiary: '#c2c2c2',
+        neutralSecondary: '#858585',
+        neutralPrimaryAlt: '#4b4b4b',
+        neutralPrimary: '#333333',
+        neutralDark: '#272727',
+        black: '#1d1d1d',
+        white: '#ffffff'
+    }
+});
+
+let theme = styling.getTheme();
+
+module.exports = {
+    themePrimary: theme.palette.themePrimary,
+    themeLighterAlt: theme.palette.themeLighterAlt,
+    themeLighter: theme.palette.themeLighter,
+    themeLight: theme.palette.themeLight,
+    themeTertiary: theme.palette.themeTertiary,
+    themeSecondary: theme.palette.themeSecondary,
+    themeDarkAlt: theme.palette.themeDarkAlt,
+    themeDark: theme.palette.themeDark,
+    themeDarker: theme.palette.themeDarker,
+    neutralLighterAlt: theme.palette.neutralLighterAlt,
+    neutralLighter: theme.palette.neutralLighter,
+    neutralLight: theme.palette.neutralLight,
+    neutralQuaternaryAlt: theme.palette.neutralQuaternaryAlt,
+    neutralQuaternary: theme.palette.neutralQuaternary,
+    neutralTertiaryAlt: theme.palette.neutralTertiaryAlt,
+    neutralTertiary: theme.palette.neutralTertiary,
+    neutralSecondary: theme.palette.neutralSecondary,
+    neutralPrimaryAlt: theme.palette.neutralPrimaryAlt,
+    neutralPrimary: theme.palette.neutralPrimary,
+    neutralDark: theme.palette.neutralDark,
+    black: theme.palette.black,
+    white: theme.palette.white
+};
