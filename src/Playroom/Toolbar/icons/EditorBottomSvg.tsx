74ada47f557e76ee478c398f30cbd49a32395aa3
Michael Taranto <mtaranto@seek.com.au>
2019-12-19 12:11:07 +1100

    refactor: Optimise svg icons

--- src/Playroom/Toolbar/icons/EditorBottomSvg.tsx
+++ src/Playroom/Toolbar/icons/EditorBottomSvg.tsx
@@ -2,13 +2,13 @@ import React, { SVGProps } from 'react';
 
 export default (props: SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
+    focusable="false"
     fill="currentColor"
     {...props}
   >
-    <path d="M19,4H5A3.00328,3.00328,0,0,0,2,7V17a3.00328,3.00328,0,0,0,3,3H19a3.00328,3.00328,0,0,0,3-3V7A3.00328,3.00328,0,0,0,19,4Zm1,10H4V7A1.00067,1.00067,0,0,1,5,6H19a1.00067,1.00067,0,0,1,1,1Z" />
+    <path d="M19 4H5a3.003 3.003 0 00-3 3v10a3.003 3.003 0 003 3h14a3.003 3.003 0 003-3V7a3.003 3.003 0 00-3-3zm1 10H4V7a1 1 0 011-1h14a1 1 0 011 1z" />
   </svg>
 );
--- src/Playroom/Toolbar/icons/EditorRightSvg.tsx
+++ src/Playroom/Toolbar/icons/EditorRightSvg.tsx
@@ -2,13 +2,13 @@ import React, { SVGProps } from 'react';
 
 export default (props: SVGProps<SVGSVGElement>) => (
   <svg
-    xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
     viewBox="0 0 24 24"
+    focusable="false"
     fill="currentColor"
     {...props}
   >
-    <path d="M19,4H5A3.00328,3.00328,0,0,0,2,7V17a3.00328,3.00328,0,0,0,3,3H19a3.00328,3.00328,0,0,0,3-3V7A3.00328,3.00328,0,0,0,19,4ZM5,18a1.00067,1.00067,0,0,1-1-1V7A1.00067,1.00067,0,0,1,5,6H16V18Z" />
+    <path d="M19 4H5a3.003 3.003 0 00-3 3v10a3.003 3.003 0 003 3h14a3.003 3.003 0 003-3V7a3.003 3.003 0 00-3-3zM5 18a1 1 0 01-1-1V7a1 1 0 011-1h11v12z" />
   </svg>
 );
