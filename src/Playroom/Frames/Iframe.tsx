b356900905beca32e714f697fb0857602ca90833
mhensley-nw <mhensley@nerdwallet.com>
2020-05-13 14:56:07 -0700

    Webpack config + iframe sandboxing

--- src/Playroom/Frames/Iframe.tsx
+++ src/Playroom/Frames/Iframe.tsx
@@ -12,12 +12,14 @@ interface IframeProps extends AllHTMLAttributes<HTMLIFrameElement> {
   intersectionRootRef: MutableRefObject<Element | null>;
 }
 
+const playroomConfig = (window.__playroomConfig__ = __PLAYROOM_GLOBAL__CONFIG__);
+
 export default function Iframe({
-  intersectionRootRef,
-  style,
-  src,
-  ...restProps
-}: IframeProps) {
+                                 intersectionRootRef,
+                                 style,
+                                 src,
+                                 ...restProps
+                               }: IframeProps) {
   const [loaded, setLoaded] = useState(false);
   const [renderedSrc, setRenderedSrc] = useState<string | null>(null);
   const iframeRef = useRef<HTMLIFrameElement | null>(null);
@@ -52,6 +54,7 @@ export default function Iframe({
   return (
     <iframe
       ref={iframeRef}
+      sandbox={playroomConfig.iframeSandbox ?? "allow-scripts"}
       onLoad={() => setLoaded(true)}
       onMouseEnter={() => {
         if (src !== renderedSrc) {
