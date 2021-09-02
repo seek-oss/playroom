9ad4af495ec175a1ca60322fd2b298aeff84de58
Michael Taranto <michaeltaranto@users.noreply.github.com>
2021-09-02 12:00:11 +1000

    fix: Ensure preview mode loads static assets correctly (#234)

--- cypress/integration/smoke.js
+++ cypress/integration/smoke.js
@@ -1,11 +1,16 @@
 import { getFirstFrame, loadPlayroom } from '../support/utils';
 
 describe('Smoke', () => {
-  beforeEach(() => {
+  it('frames are interactive', () => {
     loadPlayroom();
+    getFirstFrame().click('center');
   });
 
-  it('frames are interactive', () => {
-    getFirstFrame().click('center');
+  it('preview mode loads correctly', () => {
+    cy.visit(
+      'http://localhost:9000/preview#?code=N4Igxg9gJgpiBcIA8AxCEB8r1YEIEMAnAei2LUyXJxAF8g'
+    )
+      .get('body')
+      .then((el) => expect(el.get(0).innerText).to.eq('Foo\nFoo\nBar'));
   });
 });
