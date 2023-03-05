bcf5fdce7545a383e2ca0a8b92e291edd4d174d3
Ben Jervis <bjervis@seek.com.au>
2023-03-05 21:30:08 +1100

    Add a preview link to the GitHub action summary (#276)
    * Add github actions helper
    
    * Also write the preview URL to a message
    
    * Use a link instead of p
    
    * Upgrade pnpm setup action to remove warnings

--- scripts/postCommitStatus.js
+++ scripts/postCommitStatus.js
@@ -1,4 +1,13 @@
 /* eslint-disable no-console */
+const core = require('@actions/core');
+
+const writeSummary = async ({ title, link }) => {
+  core.summary.addHeading(title, 3);
+  core.summary.addLink(link, link);
+
+  await core.summary.write();
+};
+
 (async () => {
   try {
     console.log('Posting commit status to GitHub...');
@@ -16,17 +25,24 @@
       auth: GITHUB_TOKEN,
     });
 
+    const previewUrl = `https://playroom--${GITHUB_SHA}.surge.sh`;
+
     await octokit.repos.createCommitStatus({
       owner: 'seek-oss',
       repo: 'playroom',
       sha: GITHUB_SHA,
       state: 'success',
       context: 'Preview Site',
-      target_url: `https://playroom--${GITHUB_SHA}.surge.sh`,
+      target_url: previewUrl,
       description: 'The preview for this PR has been successfully deployed',
     });
 
     console.log('Successfully posted commit status to GitHub');
+
+    await writeSummary({
+      title: 'Preview published',
+      link: previewUrl,
+    });
   } catch (err) {
     console.error(err);
     process.exit(1); // eslint-disable-line no-process-exit
