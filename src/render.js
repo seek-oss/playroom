15502d747144230217d109249352b12e00c4082a
Remus Mate <rmate@seek.com.au>
2023-07-10 14:50:20 +1000

    revert some changes

--- src/render.js
+++ src/render.js
@@ -1,6 +1,19 @@
-import { createRoot } from 'react-dom/client';
+import ReactDOM, { version as reactDomVersion } from 'react-dom';
+
+// Uses the correct render API based on the available version of
+// `react-dom`. This hack can be removed when support for older
+// versions of React is removed.
+const canUseNewReactRootApi =
+  reactDomVersion &&
+  (reactDomVersion.startsWith('18') || reactDomVersion.startsWith('0.0.0'));
 
 export const renderElement = (node, outlet) => {
-  const root = createRoot(outlet);
-  root.render(node);
+  if (canUseNewReactRootApi) {
+    // eslint-disable-next-line import/no-unresolved
+    const { createRoot } = require('react-dom/client');
+    const root = createRoot(outlet);
+    root.render(node);
+  } else {
+    ReactDOM.render(node, outlet);
+  }
 };
