1b00e531b3ffb4f25494cf277bb3fdb7b03c30af
Adam Skoufis <adam.skoufis@gmail.com>
2023-11-12 14:54:07 +1100

    Replace `@babel/standalone` with `sucrase` for jsx compilation

--- src/index.d.ts
+++ src/index.d.ts
@@ -15,6 +15,7 @@ interface PlayroomConfig {
   iframeSandbox?: string;
   // eslint-disable-next-line @typescript-eslint/consistent-type-imports
   reactDocgenTypescriptConfig?: import('react-docgen-typescript').ParserOptions;
+  production?: boolean;
 }
 
 interface InternalPlayroomConfig extends PlayroomConfig {
