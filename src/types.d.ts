f5083292126c2e2b22b17bc074fe03579c173e5a
Victor Tortolero <victormtortolero@gmail.com>
2023-11-13 21:05:38 -0600

    move import

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,4 @@
+import type { ParserOptions } from 'react-docgen-typescript';
 import type { ParamType } from './utils/url';
 
 // all of the types in here get exposed as part of the public API
@@ -26,5 +27,5 @@ export interface PlayroomConfig {
   paramType: ParamType;
   iframeSandbox?: string;
   // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-  reactDocgenTypescriptConfig?: import('react-docgen-typescript').ParserOptions;
+  reactDocgenTypescriptConfig?: ParserOptions;
 }
