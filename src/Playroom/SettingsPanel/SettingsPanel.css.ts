a3729bb111ebc0e0504164b823e8e2bd772d0b55
Felix Habib <fhabib@seek.com.au>
2024-02-28 14:20:38 +1100

    Move tab title field from settings panel to frames panel

--- src/Playroom/SettingsPanel/SettingsPanel.css.ts
+++ src/Playroom/SettingsPanel/SettingsPanel.css.ts
@@ -99,21 +99,3 @@ export const label = style([
   },
 ]);
 
-export const textField = style([
-  sprinkles({
-    font: 'large',
-    width: 'full',
-    paddingX: 'large',
-    boxSizing: 'border-box',
-    borderRadius: 'medium',
-  }),
-  {
-    color: colorPaletteVars.foreground.neutral,
-    height: vars.touchableSize,
-    background: colorPaletteVars.background.surface,
-    '::placeholder': {
-      color: colorPaletteVars.foreground.neutralSoft,
-    },
-    border: `1px solid ${colorPaletteVars.border.standard}`,
-  },
-]);
