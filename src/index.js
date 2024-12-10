23470cdb472f16d60c5ef1fb47d4cd49d24a6b6d
Adam Skoufis <askoufis@seek.com.au>
2024-12-10 11:13:54 +1100

    Remove `IntersectionObserver` polyfill

--- src/index.js
+++ src/index.js
@@ -5,67 +5,58 @@ import playroomConfig from './config';
 import faviconPath from '../images/favicon.png';
 import faviconInvertedPath from '../images/favicon-inverted.png';
 
-const polyfillIntersectionObserver = () =>
-  typeof window.IntersectionObserver !== 'undefined'
-    ? Promise.resolve()
-    : import('intersection-observer');
+const widths = playroomConfig.widths || [320, 375, 768, 1024];
 
-polyfillIntersectionObserver().then(() => {
-  const widths = playroomConfig.widths || [320, 375, 768, 1024];
+const outlet = document.createElement('div');
+document.body.appendChild(outlet);
 
-  const outlet = document.createElement('div');
-  document.body.appendChild(outlet);
+const selectedElement = document.head.querySelector('link[rel="icon"]');
+const favicon = window.matchMedia('(prefers-color-scheme: dark)').matches
+  ? faviconInvertedPath
+  : faviconPath;
 
-  const selectedElement = document.head.querySelector('link[rel="icon"]');
-  const favicon = window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? faviconInvertedPath
-    : faviconPath;
+if (selectedElement) {
+  selectedElement.setAttribute('href', favicon);
+}
 
-  if (selectedElement) {
-    selectedElement.setAttribute('href', favicon);
-  }
+const renderPlayroom = ({
+  themes = require('./themes'),
+  components = require('./components'),
+  snippets = require('./snippets'),
+} = {}) => {
+  const themeNames = Object.keys(themes);
 
-  const renderPlayroom = ({
-    themes = require('./themes'),
-    components = require('./components'),
-    snippets = require('./snippets'),
-  } = {}) => {
-    const themeNames = Object.keys(themes);
+  // Exclude undefined components, e.g. an exported TypeScript type.
+  const filteredComponents = Object.fromEntries(
+    Object.entries(components).filter(([_, value]) => value)
+  );
 
-    // Exclude undefined components, e.g. an exported TypeScript type.
-    const filteredComponents = Object.fromEntries(
-      Object.entries(components).filter(([_, value]) => value)
-    );
+  renderElement(
+    <StoreProvider themes={themeNames} widths={widths}>
+      <Playroom
+        components={filteredComponents}
+        widths={widths}
+        themes={themeNames}
+        snippets={
+          typeof snippets.default !== 'undefined' ? snippets.default : snippets
+        }
+      />
+    </StoreProvider>,
+    outlet
+  );
+};
+renderPlayroom();
 
-    renderElement(
-      <StoreProvider themes={themeNames} widths={widths}>
-        <Playroom
-          components={filteredComponents}
-          widths={widths}
-          themes={themeNames}
-          snippets={
-            typeof snippets.default !== 'undefined'
-              ? snippets.default
-              : snippets
-          }
-        />
-      </StoreProvider>,
-      outlet
-    );
-  };
-  renderPlayroom();
+if (module.hot) {
+  module.hot.accept('./components', () => {
+    renderPlayroom({ components: require('./components') });
+  });
 
-  if (module.hot) {
-    module.hot.accept('./components', () => {
-      renderPlayroom({ components: require('./components') });
-    });
+  module.hot.accept('./themes', () => {
+    renderPlayroom({ themes: require('./themes') });
+  });
 
-    module.hot.accept('./themes', () => {
-      renderPlayroom({ themes: require('./themes') });
-    });
-
-    module.hot.accept('./snippets', () => {
-      renderPlayroom({ snippets: require('./snippets') });
-    });
-  }
-});
+  module.hot.accept('./snippets', () => {
+    renderPlayroom({ snippets: require('./snippets') });
+  });
+}
