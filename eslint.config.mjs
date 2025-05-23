646c21bba18bbe872bf7832251f2437e91aad8f5
Felix Habib <fhabib@seek.com.au>
2025-05-23 11:26:28 +1000

    Apply import order rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,6 +8,29 @@ export default defineConfig([
     rules: {
       'no-console': 0,
       'no-process-exit': 0,
+      'import-x/order': [
+        'error',
+        {
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc' },
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+          ],
+          pathGroups: [
+            {
+              pattern: '*.css',
+              group: 'index',
+              position: 'after',
+              patternOptions: { matchBase: true },
+            },
+          ],
+        },
+      ],
     },
   },
   {
