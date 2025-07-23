a8438e07ef4e1cdb597fab89b9090ab0ff1d19da
Michael Taranto <michael.taranto@gmail.com>
2025-07-23 15:47:47 +1000

    Reduce thickness of resize handle

--- src/components/Playroom/ResizeHandle.css.ts
+++ src/components/Playroom/ResizeHandle.css.ts
@@ -2,7 +2,7 @@ import { style, styleVariants } from '@vanilla-extract/css';
 
 import { colorPaletteVars, sprinkles } from '../../css/sprinkles.css';
 
-const thickness = 8;
+const thickness = 4;
 const length = 70;
 
 // Separate style applied to the container AND the body during resize.
