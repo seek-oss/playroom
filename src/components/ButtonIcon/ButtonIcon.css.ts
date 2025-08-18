002c8afb4512139d74246c9a39c434933c6a8d45
Michael Taranto <michael.taranto@gmail.com>
2025-08-18 15:21:46 +1000

    Add internal ButtonIcon component

---
+++ src/components/ButtonIcon/ButtonIcon.css.ts
@@ -0,0 +1,88 @@
+import {
+  createVar,
+  globalStyle,
+  style,
+  styleVariants,
+} from '@vanilla-extract/css';
+import { calc } from '@vanilla-extract/css-utils';
+
+import {
+  minTouchableBeforePseudo,
+  sharedPopupStyles,
+} from '../../css/shared.css';
+import { colorPaletteVars, sprinkles } from '../../css/sprinkles.css';
+import { vars } from '../../css/vars.css';
+
+const sizeVar = createVar();
+export const button = style([
+  sprinkles({
+    boxSizing: 'border-box',
+    margin: 'none',
+    padding: 'none',
+    userSelect: 'none',
+    border: 0,
+    appearance: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'relative',
+    transition: 'fast',
+  }),
+  minTouchableBeforePseudo,
+  {
+    background: 'transparent',
+    outline: 'none',
+    color: colorPaletteVars.foreground.neutral,
+    height: sizeVar,
+    width: sizeVar,
+    isolation: 'isolate',
+    // Background pseudo element
+    '::after': {
+      content: '',
+      position: 'absolute',
+      inset: calc(vars.space.xsmall).negate().toString(),
+      transition: vars.transition.fast,
+      backgroundColor: colorPaletteVars.background.neutral,
+      borderRadius: vars.radii.full,
+      zIndex: -1,
+    },
+    selectors: {
+      [`&:not(:hover, :focus-visible, [data-popup-open])::after`]: {
+        opacity: 0,
+        transform: 'scale(.5)',
+      },
+      ['&:active']: {
+        transform: 'scale(.9)',
+      },
+      [`&:focus-visible::after`]: {
+        outline: `2px solid ${colorPaletteVars.outline.focus}`,
+      },
+    },
+  },
+]);
+
+export const size = styleVariants({
+  small: {
+    vars: {
+      [sizeVar]: '16px',
+    },
+  },
+  medium: {
+    vars: {
+      [sizeVar]: '20px',
+    },
+  },
+  large: {
+    vars: {
+      [sizeVar]: '24px',
+    },
+  },
+});
+
+globalStyle(`${button} > svg`, {
+  display: 'block',
+  height: sizeVar,
+  width: sizeVar,
+});
+
+export const popup = sharedPopupStyles('small');
