462021f333d30d57ed99f25981ada991087d7e20
Michael Taranto <michael.taranto@gmail.com>
2025-08-19 11:00:47 +1000

    Add internal Menu components

---
+++ src/components/Menu/Menu.css.ts
@@ -0,0 +1,124 @@
+import { style } from '@vanilla-extract/css';
+
+import { comma } from '../../css/delimiters';
+
+import {
+  minTouchableBeforePseudo,
+  sharedPopupStyles,
+} from '../../css/shared.css';
+import { colorPaletteVars, sprinkles } from '../../css/sprinkles.css';
+import { vars } from '../../css/vars.css';
+
+export const trigger = style([
+  sprinkles({
+    boxSizing: 'border-box',
+    margin: 'none',
+    padding: 'none',
+    userSelect: 'none',
+    font: 'small',
+    borderRadius: 'small',
+    border: 0,
+    appearance: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 'medium',
+    position: 'relative',
+  }),
+  minTouchableBeforePseudo,
+  {
+    background: 'transparent',
+    ':focus-visible': {
+      outline: `2px solid ${colorPaletteVars.outline.focus}`,
+      outlineOffset: 6,
+    },
+  },
+]);
+
+const popupGutter = 'xsmall';
+export const popup = style([
+  sharedPopupStyles('small'),
+  {
+    width: 200,
+  },
+]);
+
+export const item = style([
+  sprinkles({
+    position: 'relative',
+    userSelect: 'none',
+    font: 'standard',
+    padding: 'xsmall',
+  }),
+  {
+    color: colorPaletteVars.foreground.neutral,
+    '::before': {
+      content: '',
+      pointerEvents: 'none',
+      position: 'absolute',
+      insetBlock: 0,
+      insetInline: 0,
+      borderRadius: vars.radii.small,
+    },
+    selectors: {
+      [comma('&[data-popup-open]::before', '&[data-highlighted]::before')]: {
+        backgroundColor: colorPaletteVars.background.selection,
+      },
+      [`&[aria-disabled]`]: {
+        color: colorPaletteVars.foreground.secondary,
+      },
+    },
+  },
+]);
+
+export const submenuTrigger = style([
+  item,
+  sprinkles({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 'xsmall',
+  }),
+]);
+
+export const fieldItem = style([
+  item,
+  sprinkles({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    gap: 'xsmall',
+  }),
+]);
+
+const fieldIndicatorSize = '16px';
+export const fieldItemIndicator = style([
+  sprinkles({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }),
+  {
+    height: fieldIndicatorSize,
+    width: fieldIndicatorSize,
+    color: colorPaletteVars.foreground.neutral,
+  },
+]);
+
+export const menuGroupLabel = style([
+  item,
+  sprinkles({
+    fontWeight: 'strong',
+  }),
+]);
+
+export const separator = style([
+  sprinkles({
+    marginY: popupGutter,
+    marginX: 'xxsmall',
+  }),
+  {
+    height: 1,
+    backgroundColor: colorPaletteVars.border.standard,
+  },
+]);
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,155 @@
+import { Menu as BaseUIMenu } from '@base-ui-components/react/menu';
+import {
+  type ComponentProps,
+  createContext,
+  forwardRef,
+  type ReactNode,
+  useContext,
+} from 'react';
+
+import ChevronIcon from '../icons/ChevronIcon';
+import TickIcon from '../icons/TickIcon';
+
+import * as styles from './Menu.css';
+
+export const MenuItem = ({
+  onClick,
+  children,
+  closeOnClick,
+  disabled,
+}: {
+  onClick: ComponentProps<typeof BaseUIMenu.Item>['onClick'];
+  children: ComponentProps<typeof BaseUIMenu.Item>['children'];
+  closeOnClick?: ComponentProps<typeof BaseUIMenu.Item>['closeOnClick'];
+  disabled?: ComponentProps<typeof BaseUIMenu.Item>['disabled'];
+}) => (
+  <BaseUIMenu.Item
+    className={styles.item}
+    onClick={onClick}
+    closeOnClick={closeOnClick}
+    disabled={disabled}
+  >
+    {children}
+  </BaseUIMenu.Item>
+);
+
+export const MenuRadioGroup = BaseUIMenu.RadioGroup;
+
+export const MenuRadioItem = ({
+  value,
+  children,
+}: {
+  value: ComponentProps<typeof BaseUIMenu.RadioItem>['value'];
+  children: ComponentProps<typeof BaseUIMenu.RadioItem>['children'];
+}) => (
+  <BaseUIMenu.RadioItem className={styles.fieldItem} value={value}>
+    {children}
+    <BaseUIMenu.RadioItemIndicator className={styles.fieldItemIndicator}>
+      <svg
+        viewBox="0 0 24 24"
+        focusable="false"
+        fill="currentColor"
+        width={12}
+        height={12}
+        role="img"
+      >
+        <circle
+          cx="12"
+          cy="12"
+          r="8"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="5"
+        />
+      </svg>
+    </BaseUIMenu.RadioItemIndicator>
+  </BaseUIMenu.RadioItem>
+);
+
+export const MenuCheckboxItem = ({
+  checked,
+  onCheckedChange,
+  children,
+}: {
+  checked: ComponentProps<typeof BaseUIMenu.CheckboxItem>['checked'];
+  onCheckedChange: ComponentProps<
+    typeof BaseUIMenu.CheckboxItem
+  >['onCheckedChange'];
+  children: ComponentProps<typeof BaseUIMenu.CheckboxItem>['children'];
+}) => (
+  <BaseUIMenu.CheckboxItem
+    checked={checked}
+    onCheckedChange={onCheckedChange}
+    className={styles.fieldItem}
+  >
+    {children}
+    <BaseUIMenu.CheckboxItemIndicator className={styles.fieldItemIndicator}>
+      <TickIcon />
+    </BaseUIMenu.CheckboxItemIndicator>
+  </BaseUIMenu.CheckboxItem>
+);
+
+export const MenuSeparator = () => (
+  <BaseUIMenu.Separator className={styles.separator} />
+);
+
+export const MenuGroup = ({
+  label,
+  children,
+}: {
+  label: string;
+  children: ComponentProps<typeof BaseUIMenu.Group>['children'];
+}) => (
+  <BaseUIMenu.Group>
+    <BaseUIMenu.GroupLabel className={styles.menuGroupLabel}>
+      {label}
+    </BaseUIMenu.GroupLabel>
+    {children}
+  </BaseUIMenu.Group>
+);
+
+const SubMenuContext = createContext(false);
+
+type Props = {
+  trigger: ReactNode;
+  align?: ComponentProps<typeof BaseUIMenu.Positioner>['align'];
+  children: ComponentProps<typeof BaseUIMenu.Popup>['children'];
+};
+export const Menu = forwardRef<HTMLButtonElement, Props>(
+  ({ trigger, align = 'start', children }, triggerRef) => {
+    const isSubMenu = useContext(SubMenuContext);
+    const MenuRoot = isSubMenu ? BaseUIMenu.SubmenuRoot : BaseUIMenu.Root;
+    const MenuTrigger = isSubMenu
+      ? BaseUIMenu.SubmenuTrigger
+      : BaseUIMenu.Trigger;
+
+    return (
+      <SubMenuContext.Provider value={true}>
+        <MenuRoot>
+          <MenuTrigger
+            ref={triggerRef}
+            className={isSubMenu ? styles.submenuTrigger : styles.trigger}
+          >
+            {trigger}
+            <ChevronIcon direction={isSubMenu ? 'right' : 'down'} size={12} />
+          </MenuTrigger>
+          <BaseUIMenu.Portal>
+            <BaseUIMenu.Positioner
+              align={align}
+              alignOffset={-8 /* Align with the padding size of the popup */}
+              sideOffset={
+                isSubMenu
+                  ? 10 /* Align with the padding size of the popup + gap */
+                  : 8 /* Double padding size (medium) */
+              }
+            >
+              <BaseUIMenu.Popup className={styles.popup}>
+                {children}
+              </BaseUIMenu.Popup>
+            </BaseUIMenu.Positioner>
+          </BaseUIMenu.Portal>
+        </MenuRoot>
+      </SubMenuContext.Provider>
+    );
+  }
+);
