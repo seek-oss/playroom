53b1ebd9363ad02778d9c68bd8abc053bb92fe58
Michael Taranto <michael.taranto@gmail.com>
2025-08-19 16:24:52 +1000

    Move 'Copy link' into menu

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -1,5 +1,5 @@
 export const ANIMATION_DURATION_SLOW = 300;
 
-export const toolbarItemCount = 5;
+export const toolbarItemCount = 3;
 export const toolbarItemSize = 60;
 export const toolbarOpenSize = 320;
