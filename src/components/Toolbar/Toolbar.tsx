a9317d29bfa16439002f9ff7f0834c7878a893e7
Michael Taranto <michael.taranto@gmail.com>
2025-08-19 16:24:52 +1000

    Move 'Copy link' into menu

--- src/components/Toolbar/Toolbar.tsx
+++ src/components/Toolbar/Toolbar.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useContext, useState, useCallback, useEffect, useRef } from 'react';
+import { useContext, useState, useEffect, useRef } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 import snippets from '../../configModules/snippets';
@@ -13,7 +13,6 @@ import { ANIMATION_DURATION_SLOW } from '../constants';
 import AddIcon from '../icons/AddIcon';
 import FramesIcon from '../icons/FramesIcon';
 import SettingsIcon from '../icons/SettingsIcon';
-import ShareIcon from '../icons/ShareIcon';
 
 import * as styles from './Toolbar.css';
 
@@ -27,23 +26,6 @@ export default () => {
     },
     dispatch,
   ] = useContext(StoreContext);
-  const [copying, setCopying] = useState(false);
-  const [isReady, cancel, reset] = useTimeoutFn(() => setCopying(false), 3000);
-
-  const copyHandler = useCallback(() => {
-    dispatch({
-      type: 'copyToClipboard',
-      payload: { url: window.location.href, trigger: 'toolbarItem' },
-    });
-    setCopying(true);
-
-    if (isReady() === false) {
-      cancel();
-    }
-
-    reset();
-  }, [cancel, dispatch, isReady, reset]);
-
   const isSnippetsOpen = activeToolbarPanel === 'snippets';
   const isFramesOpen = activeToolbarPanel === 'frames';
   const isSettingsOpen = activeToolbarPanel === 'settings';
@@ -109,27 +91,18 @@ export default () => {
             </ToolbarItem>
           </div>
 
-          <div>
-            <ToolbarItem
-              title="Copy Playroom link"
-              success={copying}
-              onClick={copyHandler}
-            >
-              <ShareIcon />
-            </ToolbarItem>
-            <ToolbarItem
-              active={isSettingsOpen}
-              title="Edit settings"
-              onClick={() =>
-                dispatch({
-                  type: 'toggleToolbar',
-                  payload: { panel: 'settings' },
-                })
-              }
-            >
-              <SettingsIcon />
-            </ToolbarItem>
-          </div>
+          <ToolbarItem
+            active={isSettingsOpen}
+            title="Edit settings"
+            onClick={() =>
+              dispatch({
+                type: 'toggleToolbar',
+                payload: { panel: 'settings' },
+              })
+            }
+          >
+            <SettingsIcon />
+          </ToolbarItem>
         </div>
         <CSSTransition
           in={isOpen}
@@ -172,53 +145,3 @@ export default () => {
     </div>
   );
 };
-
-// copied directly from `react-use`
-// https://github.com/streamich/react-use/blob/db07ab65bfa48a399e7fd83f172653eb342882b1/src/useTimeoutFn.ts#L3-L40
-type UseTimeoutFnReturn = [() => boolean | null, () => void, () => void];
-
-function useTimeoutFn<T extends () => void>(
-  fn: T,
-  ms: number = 0
-): UseTimeoutFnReturn {
-  const ready = useRef<boolean | null>(false);
-  const timeout = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
-  const callback = useRef(fn);
-
-  const isReady = useCallback(() => ready.current, []);
-
-  const set = useCallback(() => {
-    ready.current = false;
-    if (timeout.current) {
-      clearTimeout(timeout.current);
-    }
-
-    timeout.current = setTimeout(() => {
-      ready.current = true;
-      callback.current();
-    }, ms);
-  }, [ms]);
-
-  const clear = useCallback(() => {
-    ready.current = null;
-    if (timeout.current) {
-      clearTimeout(timeout.current);
-    }
-  }, []);
-
-  // update ref when function changes
-  useEffect(() => {
-    callback.current = fn;
-  }, [fn]);
-
-  // set on mount, clear on unmount
-  useEffect(() => {
-    set();
-
-    return clear;
-    // disabled in the original implementation
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ms]);
-
-  return [isReady, clear, set];
-}
