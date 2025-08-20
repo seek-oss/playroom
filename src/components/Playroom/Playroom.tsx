b4fe2489088254d552964be3bb5949a1ba4fdf66
Michael Taranto <michael.taranto@gmail.com>
2025-08-20 14:46:44 +1000

    Refactor 'ready' state to store

--- src/components/Playroom/Playroom.tsx
+++ src/components/Playroom/Playroom.tsx
@@ -52,7 +52,6 @@ export default () => {
       code,
       previewRenderCode,
       previewEditorCode,
-      ready,
       title,
     },
     dispatch,
@@ -82,7 +81,7 @@ export default () => {
   const isVerticalEditor = editorPosition === 'right';
   const editorSize = isVerticalEditor ? editorWidth : editorHeight;
 
-  return !ready ? null : (
+  return (
     <Box
       component="main"
       className={{
