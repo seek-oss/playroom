d139257cac8be579f9d5164de039e8ba781fb87f
Michael Taranto <michael.taranto@gmail.com>
2025-08-29 17:12:30 +1000

    Add internal Popover component and migrate to it

---
+++ src/components/Popover/Popover.css.ts
@@ -0,0 +1,7 @@
+import { sharedPopupStyles } from '../../css/shared.css';
+
+export const size = {
+  small: sharedPopupStyles('small'),
+  medium: sharedPopupStyles('medium'),
+  large: sharedPopupStyles('large'),
+};
--- src/components/PreviewSelection/PreviewSelection.tsx
+++ src/components/PreviewSelection/PreviewSelection.tsx
@@ -12,8 +12,6 @@ import PlayIcon from '../icons/PlayIcon';
 import { CopyButton } from './CopyButton';
 import { ThemeSelector } from './ThemeSelector';
 
-import * as styles from './PreviewSelection.css';
-
 export const PreviewSelection = () => {
   const [{ selectedThemes }] = useContext(StoreContext);
   const defaultTheme =
@@ -27,34 +25,32 @@ export const PreviewSelection = () => {
   const prototypeUrl = usePreviewUrl(activeTheme);
 
   return (
-    <div className={styles.root}>
-      <Stack space="large">
-        <CopyButton copyContent={window.location.href} title="Playroom link" />
+    <Stack space="large">
+      <CopyButton copyContent={window.location.href} title="Playroom link" />
 
-        <Stack space="medium">
-          <Heading level="3">Preview</Heading>
-          {themesEnabled ? (
-            <ThemeSelector
-              selectedThemes={selectedThemes}
-              activeTheme={activeTheme}
-              onChange={setUserSelectedTheme}
-            />
-          ) : null}
-          <Inline space="small">
-            <Button
-              as="a"
-              href={prototypeUrl}
-              target="_blank"
-              title="Open preview in new window"
-              rel="noopener noreferrer"
-              icon={<PlayIcon size={20} />}
-            >
-              Open
-            </Button>
-            <CopyButton copyContent={prototypeUrl} title="Preview link" />
-          </Inline>
-        </Stack>
+      <Stack space="medium">
+        <Heading level="3">Preview</Heading>
+        {themesEnabled ? (
+          <ThemeSelector
+            selectedThemes={selectedThemes}
+            activeTheme={activeTheme}
+            onChange={setUserSelectedTheme}
+          />
+        ) : null}
+        <Inline space="small">
+          <Button
+            as="a"
+            href={prototypeUrl}
+            target="_blank"
+            title="Open preview in new window"
+            rel="noopener noreferrer"
+            icon={<PlayIcon size={20} />}
+          >
+            Open
+          </Button>
+          <CopyButton copyContent={prototypeUrl} title="Preview link" />
+        </Inline>
       </Stack>
-    </div>
+    </Stack>
   );
 };
