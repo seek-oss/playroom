011e5d5eed8c3da74d8fdd218652363e039fb18f
Felix Habib <fhabib@seek.com.au>
2025-09-04 14:59:12 +1000

    Update Preview and Embed mode

--- src/components/Preview/Preview.tsx
+++ src/components/Preview/Preview.tsx
@@ -1,39 +1,95 @@
-import { useEffect, useState, type ReactNode } from 'react';
+import clsx from 'clsx';
+import { Maximize2 } from 'lucide-react';
+import { useState } from 'react';
 
 import { useDocumentTitle } from '../../utils/useDocumentTitle';
-import { Box } from '../Box/Box';
+import { Button } from '../Button/Button';
+import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
+import { ReceiveErrorMessage } from '../Frame/frameMessaging';
+import frameSrc from '../Frames/frameSrc';
+import { Logo } from '../Logo/Logo';
+import { Text } from '../Text/Text';
 
-import SplashScreen from './SplashScreen/SplashScreen';
-
-import * as stylesheet from './SplashScreen/SplashScreen.css';
-
-const { animationDuration, animationDelay, animationIterationCount } =
-  stylesheet;
+import * as styles from './Preview.css';
 
 interface PreviewProps {
   title?: string;
-  children?: ReactNode;
+  code: string;
+  themeName: string;
 }
-export default ({ title, children }: PreviewProps) => {
-  const [loading, setLoading] = useState(true);
 
+const PreviewHeader = ({
+  editorHref,
+  onHideHeader,
+}: {
+  editorHref: string;
+  onHideHeader: () => void;
+}) => (
+  <div className={styles.header}>
+    <div className={styles.leftGroup}>
+      <Logo size={20} />
+      <Text tone="secondary">
+        Preview containing unreviewed, user-provided code.
+      </Text>
+    </div>
+    <div className={styles.actions}>
+      <Button as="a" href={editorHref} target="_blank" rel="noreferrer">
+        Edit
+      </Button>
+      <ButtonIcon
+        label="Hide header"
+        icon={<Maximize2 />}
+        onClick={onHideHeader}
+      />
+    </div>
+  </div>
+);
+
+const PreviewFooter = ({ editorHref }: { editorHref: string }) => (
+  <div className={styles.footer}>
+    <div className={styles.leftGroup}>
+      <Logo size={20} />
+      <Text tone="secondary">Playroom</Text>
+    </div>
+    <div className={styles.actions}>
+      <Button as="a" href={editorHref} target="_blank" rel="noreferrer">
+        Open
+      </Button>
+    </div>
+  </div>
+);
+
+export default ({ title, code, themeName }: PreviewProps) => {
   useDocumentTitle({ title, suffix: 'Playroom Preview' });
 
-  useEffect(() => {
-    const hideSplash = setTimeout(
-      () => setLoading(false),
-      animationDelay + animationDuration * animationIterationCount
-    );
+  const [headerHidden, setHeaderHidden] = useState(false);
+  const absoluteSrc = new URL(
+    frameSrc({ themeName, code }),
+    window.location.origin
+  ).toString();
 
-    return () => clearTimeout(hideSplash);
-  }, []);
+  const editorHref = window.location.href.replace('/preview', '');
+  const isEmbedded =
+    typeof window !== 'undefined' && window.self !== window.top;
 
   return (
-    <>
-      <Box position="relative" zIndex={0} aria-hidden={loading || undefined}>
-        {children}
-      </Box>
-      <SplashScreen hide={!loading} />
-    </>
+    <div
+      className={clsx([
+        styles.root,
+        isEmbedded ? styles.rootEmbedded : undefined,
+      ])}
+    >
+      {!headerHidden && !isEmbedded ? (
+        <PreviewHeader
+          editorHref={editorHref}
+          onHideHeader={() => setHeaderHidden(true)}
+        />
+      ) : null}
+      <div className={styles.frameContainer}>
+        <iframe src={absoluteSrc} className={styles.iframe} />
+        <ReceiveErrorMessage size="large" />
+      </div>
+      {isEmbedded ? <PreviewFooter editorHref={editorHref} /> : null}
+    </div>
   );
 };
