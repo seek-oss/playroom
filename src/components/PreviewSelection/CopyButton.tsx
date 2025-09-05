fd758177491779a279499d8dd83669d4a549d74b
Michael Taranto <michael.taranto@gmail.com>
2025-09-05 11:25:00 +1000

    Extract useCopy util to share between actions

--- src/components/PreviewSelection/CopyButton.tsx
+++ src/components/PreviewSelection/CopyButton.tsx
@@ -1,6 +1,4 @@
-import copy from 'copy-to-clipboard';
-import { useEffect, useRef, useState } from 'react';
-
+import { useCopy } from '../../utils/useCopy';
 import { Button } from '../Button/Button';
 import ShareIcon from '../icons/ShareIcon';
 import TickIcon from '../icons/TickIcon';
@@ -10,32 +8,11 @@ interface CopyButtonProps {
   title?: string;
 }
 export const CopyButton = ({ copyContent, title }: CopyButtonProps) => {
-  const [copying, setCopying] = useState(false);
-  const copyLocked = useRef(false);
-
-  useEffect(() => {
-    if (copying && !copyLocked.current) {
-      copyLocked.current = true;
-
-      copy(copyContent);
-
-      setTimeout(() => {
-        copyLocked.current = false;
-
-        setCopying(false);
-      }, 2000);
-    }
-  }, [copying, copyContent]);
-
-  const onClick = () => {
-    if (!copying) {
-      setCopying(true);
-    }
-  };
+  const { copying, onCopyClick } = useCopy();
 
   return (
     <Button
-      onClick={onClick}
+      onClick={() => onCopyClick(copyContent)}
       tone={copying ? 'positive' : undefined}
       icon={copying ? <TickIcon size={18} /> : <ShareIcon size={18} />}
     >
