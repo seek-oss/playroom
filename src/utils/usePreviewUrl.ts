0593cac44d46232371ef543d69fb43e1cf2f53b1
Michael Taranto <michael.taranto@gmail.com>
2025-10-17 10:26:13 +1100

    Header: Align preview url creation

--- src/utils/usePreviewUrl.ts
+++ src/utils/usePreviewUrl.ts
@@ -2,21 +2,20 @@ import { useContext } from 'react';
 
 import { createPreviewUrl } from '../../utils';
 import playroomConfig from '../config';
+import { themesEnabled } from '../configModules/themes';
 import { StoreContext } from '../contexts/StoreContext';
 
 const baseUrl = window.location.href
   .split(playroomConfig.paramType === 'hash' ? '#' : '?')[0]
   .split('index.html')[0];
 
-export default (theme: string) => {
+export default (theme?: string) => {
   const [{ code, title, editorHidden }] = useContext(StoreContext);
-
-  const isThemed = theme !== '__PLAYROOM__NO_THEME__';
 
   return createPreviewUrl({
     baseUrl,
     code,
-    theme: isThemed ? theme : undefined,
+    theme: themesEnabled ? theme : undefined,
     paramType: playroomConfig.paramType,
     title,
     editorHidden,
