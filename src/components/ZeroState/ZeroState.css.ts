bcb6b3ef89fcca863e0453c10ce78fa7760083c5
Michael Taranto <michael.taranto@gmail.com>
2025-10-23 15:40:32 +1100

    ZeroState: Safe center

--- src/components/ZeroState/ZeroState.css.ts
+++ src/components/ZeroState/ZeroState.css.ts
@@ -3,32 +3,33 @@ import { style } from '@vanilla-extract/css';
 import { minTouchableBeforePseudo } from '../../css/shared.css';
 import { colorPaletteVars, sprinkles } from '../../css/sprinkles.css';
 
+const verticalPadding = 'xxxlarge';
 export const root = style([
   sprinkles({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
     height: 'full',
+    paddingY: verticalPadding,
+    boxSizing: 'border-box',
   }),
   {
     background: `radial-gradient(${colorPaletteVars.background.surface} 20%, transparent 50%)`,
+    justifyContent: 'safe center',
   },
 ]);
 
 export const maxWidth = style([
-  sprinkles({ width: 'full', padding: 'medium', boxSizing: 'border-box' }),
+  sprinkles({
+    width: 'full',
+    // Workaround for scrolling flex container not accounting for padding
+    paddingBottom: verticalPadding,
+    boxSizing: 'border-box',
+  }),
   {
     maxWidth: 400,
   },
 ]);
-
-export const recentTitleSpread = sprinkles({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  gap: 'small',
-});
 
 export const textButton = style([
   sprinkles({
