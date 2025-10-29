b7520d93df507d96c1098bb6b8eb2774e17870c5
Felix Habib <33821218+felixhabib@users.noreply.github.com>
2025-10-29 15:11:57 +1100

    Ensure "Toggle comment" command works without other code

--- cypress/e2e/keymaps.cy.ts
+++ cypress/e2e/keymaps.cy.ts
@@ -549,6 +549,15 @@ describe('Keymaps', () => {
     const modifierKey = isMac() ? 'cmd' : 'ctrl';
     const executeToggleCommentCommand = () => typeCode(`{${modifierKey}+/}`);
 
+    it('should create a comment when there is no code in the editor', () => {
+      loadPlayroom('');
+      executeToggleCommentCommand();
+
+      assertCodePaneContains(dedent`
+        {/*  */}
+      `);
+    });
+
     describe('should wrap a single line in a comment when there is no selection', () => {
       it('block', () => {
         loadPlayroom(blockStarter);
