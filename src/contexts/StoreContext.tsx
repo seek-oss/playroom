e66aa82dee498ac082577fc6d5e514245d4caced
Marcos Luis Delgado <mluis@tuenti.com>
2026-01-15 12:26:42 +0000

    master: Fallback for randomUUID when not available

--- src/contexts/StoreContext.tsx
+++ src/contexts/StoreContext.tsx
@@ -189,7 +189,16 @@ const sortStoredPlayrooms = (storedPlayrooms: State['storedPlayrooms']) =>
     )
   );
 
-const createPlayroomId = () => self.crypto.randomUUID();
+const createPlayroomId = () =>
+  self.crypto.randomUUID
+    ? self.crypto.randomUUID()
+    : '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c) =>
+        (
+          Number(c) ^
+          (crypto.getRandomValues(new Uint8Array(1))[0] &
+            (15 >> (Number(c) / 4)))
+        ).toString(16)
+      );
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
