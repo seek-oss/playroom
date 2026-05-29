ec3e13d0c4ec747082d3fee815bdabf33eb66153
Mihkel Eidast <mihkel@eida.st>
2026-05-29 16:22:45 +0300

    tsdown bundle

---
+++ tsdown.src.config.ts
@@ -0,0 +1,31 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+  entry: {
+    index: 'src/entries/index.tsx',
+    frame: 'src/entries/frame.tsx',
+    preview: 'src/entries/preview.tsx',
+    'defaultModules/FrameComponent': 'src/defaultModules/FrameComponent.tsx',
+    'defaultModules/snippets': 'src/defaultModules/snippets.ts',
+    'defaultModules/themes': 'src/defaultModules/themes.ts',
+    'defaultModules/useScope': 'src/defaultModules/useScope.ts',
+  },
+  format: ['esm'],
+  dts: true,
+  clean: false,
+  outDir: 'dist/src',
+  exports: false,
+  platform: 'browser',
+  plugins: [vanillaExtractPlugin()],
+  // Doesn't affect the bundle but suppresses a warning we don't care about
+  copy: [{ from: 'images', to: 'dist' }],
+  external: [
+    /\.png/,
+    '__PLAYROOM_ALIAS__FRAME_COMPONENT__',
+    '__PLAYROOM_ALIAS__THEMES__',
+    '__PLAYROOM_ALIAS__SNIPPETS__',
+    '__PLAYROOM_ALIAS__USE_SCOPE__',
+    '__PLAYROOM_ALIAS__COMPONENTS__',
+  ],
+});
