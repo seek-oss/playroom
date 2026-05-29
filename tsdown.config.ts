ca3fb00c962efc8570da8cdaae84df64772470aa
Mihkel Eidast <mihkel@eida.st>
2026-05-29 16:22:45 +0300

    use one tsdown config file with multiple configurations

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,41 @@
+import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
 import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-  entry: ['utils/index.ts'],
-  format: ['cjs', 'esm'],
-  dts: true,
-  outDir: 'dist',
-  exports: true,
-  // Doesn't affect the bundle but suppresses a warning we don't care about
-  external: ['__PLAYROOM_ALIAS__FRAME_COMPONENT__'],
-});
+export default defineConfig([
+  {
+    entry: ['utils/index.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    outDir: 'dist',
+    exports: true,
+    // Doesn't affect the bundle but suppresses a warning we don't care about
+    external: ['__PLAYROOM_ALIAS__FRAME_COMPONENT__'],
+  },
+  {
+    entry: {
+      index: 'src/entries/index.tsx',
+      frame: 'src/entries/frame.tsx',
+      preview: 'src/entries/preview.tsx',
+      'defaultModules/FrameComponent': 'src/defaultModules/FrameComponent.tsx',
+      'defaultModules/snippets': 'src/defaultModules/snippets.ts',
+      'defaultModules/themes': 'src/defaultModules/themes.ts',
+      'defaultModules/useScope': 'src/defaultModules/useScope.ts',
+    },
+    format: ['esm'],
+    dts: false,
+    outDir: 'app',
+    exports: false,
+    platform: 'browser',
+    plugins: [vanillaExtractPlugin()],
+    copy: ['src/entries/template.html'],
+    // Doesn't affect the bundle but suppresses a warning we don't care about
+    external: [
+      /\.png/,
+      '__PLAYROOM_ALIAS__FRAME_COMPONENT__',
+      '__PLAYROOM_ALIAS__THEMES__',
+      '__PLAYROOM_ALIAS__SNIPPETS__',
+      '__PLAYROOM_ALIAS__USE_SCOPE__',
+      '__PLAYROOM_ALIAS__COMPONENTS__',
+    ],
+  },
+]);
